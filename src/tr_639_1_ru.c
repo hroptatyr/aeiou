ec725e1f3b76553bdade6d9517bfa95b5a406d9c
Sebastian Freundt <freundt@ga-group.nl>
2017-03-27 12:45:26 +0000

    chore, import russian transliterator based on github.com/tjbladez/translit

---
+++ src/tr_639_1_ru.c
@@ -0,0 +1,96 @@
+/*** tr_639_1_ru.c -- Russian (ISO 639-1 RU) language transliterations
+ *
+ * Copyright (C) 2014-2017 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of aeiou.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+/* compile with translcc */
+
+	[0x0410] = "A",
+	[0x0430] = "a",
+	[0x0411] = "B",
+	[0x0431] = "b",
+	[0x0412] = "V",
+	[0x0432] = "v",
+	[0x0413] = "G",
+	[0x0433] = "g",
+	[0x0414] = "D",
+	[0x0434] = "d",
+	[0x0415] = "E",
+	[0x0435] = "e",
+	[0x0401] = "Yo",
+	[0x0451] = "yo",
+	[0x0416] = "Zh",
+	[0x0436] = "zh",
+	[0x0417] = "Z",
+	[0x0437] = "z",
+	[0x0418] = "I",
+	[0x0438] = "i",
+	[0x0419] = "J",
+	[0x0439] = "j",
+	[0x041a] = "\x00\x00\x04\x41X\x00\x00\x00K",
+	[0x043a] = "\x00\x00\x04\x41x\x00\x00\x00k",
+	[0x041b] = "L",
+	[0x043b] = "l",
+	[0x041c] = "M",
+	[0x043c] = "m",
+	[0x041d] = "N",
+	[0x043d] = "n",
+	[0x041e] = "O",
+	[0x043e] = "o",
+	[0x041f] = "P",
+	[0x043f] = "p",
+	[0x0420] = "R",
+	[0x0440] = "r",
+	[0x0421] = "S",
+	[0x0441] = "s",
+	[0x0422] = "T",
+	[0x0442] = "t",
+	[0x0423] = "U",
+	[0x0443] = "u",
+	[0x0424] = "F",
+	[0x0444] = "f",
+	[0x0425] = "H",
+	[0x0445] = "h",
+	[0x0426] = "Ts",
+	[0x0446] = "ts",
+	[0x0429] = "Shch",
+	[0x0449] = "shch",
+	[0x042b] = "Y",
+	[0x044b] = "y",
+	[0x042d] = "Je",
+	[0x044d] = "je",
+	[0x042e] = "Yu",
+	[0x044e] = "yu",
+	[0x042f] = "Ya",
+	[0x044f] = "ya",
