f9a8c3accb2f6edec069ff7cb31efacaa709a7ad
Sebastian Freundt <freundt@ga-group.nl>
2017-03-30 09:45:57 +0000

    hygiene, be neutral about version string holder, kill nmck (numcheck) prefix

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDED_version_h_
 #define INCLUDED_version_h_
 
-extern const char nmck_version_string[];
-#define package_version	nmck_version_string
+extern const char _version_string[];
+#define package_version	_version_string
 
 #endif	/* INCLUDED_version_h_ */
